cd947fe3d7470c6d39ab6bb1c791d3f6c565a1b7
kentjmv <kentjan.viernes@gmail.com>
2022-10-27 13:56:52 +0000

    update

---
+++ gui.py
@@ -0,0 +1,54 @@
+from tkinter import *
+from tkinter import messagebox
+import urllib.parse
+import requests
+
+main_api = "https://www.mapquestapi.com/directions/v2/route?"
+key = "RJ2gn0AMzZ8Q2Okaf2GQwetvvKLuyYrS"
+
+def submit():
+    location= entry1.get()
+    destination= entry2.get()
+    x = 1.61
+
+    if (location=="" and destination==""):
+         messagebox.showinfo("","Please Input Location or Destination")
+
+    while True:
+        url = main_api + urllib.parse.urlencode({"key": key, "from":location, "to":destination})
+        json_data = requests.get(url).json()
+        json_status = json_data["info"]["statuscode"]
+        if json_status == 0:
+                ml1 = Label(root,text="API Status: " + str(json_status) + " = A successful route call.\n").place(x=20,y=150)
+                ml2 = Label(root,text="Directions from " + (location + " to " + (destination))).place(x=20,y=180)
+                ml3 = Label(root,text="Time Duration: " + (json_data["route"]["formattedTime"])).place(x=20,y=220)
+                ml4 = Label(root,text="Distance: " + str("{:.2f}".format((json_data["route"]["distance"])*x))).place(x=20,y=260)
+    
+    else:
+        messagebox.showinfo("","Unknown Information!")
+
+# def route():
+#     lbl = Label(root,text="Alternative Route")
+
+root=Tk()
+root.title("Mapquest")
+root.geometry("500x500")
+
+
+global entry1
+global entry2
+
+Label(root,text="Location").place(x=20,y=20)
+Label(root,text="Destination").place(x=20,y=60)
+
+entry1=Entry(root,bd=1)
+entry1.place(x=150,y=20)
+
+entry2=Entry(root,bd=1)
+entry2.place(x=150,y=60)
+
+Button(root,text="Submit",command=submit,height=1,width=10,bd=5).place(x=150,y=100)
+
+# Button(root,text="Alternative Route",command=route,height=1,width=12,bd=5).place(x=150,y=240)
+
+root.mainloop()
